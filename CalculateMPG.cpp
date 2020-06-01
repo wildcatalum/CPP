8cf8059ebd02d134d4f3af4f6318b7939d85ffea
wildcatalum <efrenibarra@email.arizona.edu>
2020-06-01 10:57:36 -0700

    first code in assignment

---
+++ CalculateMPG.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+{
+    double mpg,
+        miles = 375,
+        gallons = 15;
+
+    mpg = miles / gallons ;
+
+    cout << mpg;
+    return 0;
+}
