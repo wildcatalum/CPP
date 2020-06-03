0b05cea4b18978c9caed4df91f1078341e94196b
wildcatalum <efrenibarra@email.arizona.edu>
2020-06-03 12:26:30 -0700

    completed third assignment

---
+++ CustReceipt.cpp
@@ -0,0 +1,27 @@
+//Assignment 3 - Customer Receipt
+///Purpose: This program prints a customer receipt that calculates total price from a fixed sales tax and arbitrary item price
+///Input: itemprice, item, salestax
+///Process: tax = itemprice * salestax, totalprice = itemprice + tax 
+///Output: Print message "Customer Receipt: $totalprice, Price: 10.00 + Tax: 0.83, for product item"
+
+//psuedocode
+///Store variable data
+///Calculate tax
+///Calculate totalprice
+///Display ""Customer Receipt: $totalprice, Price: 10.00 + Tax: 0.83, for product item""
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string item = "Flower";
+    double salestax = 0.0825, itemprice = 32.00;
+    double tax = itemprice * salestax;
+    double totalprice = itemprice + tax;
+
+    cout << "Customer Receipt: $" << totalprice << ", Price: " << itemprice << " + Tax: " << tax << ", for product " << item;
+    return 0;
+
+}
