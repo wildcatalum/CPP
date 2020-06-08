72366a26273c1c99c508ed0b8191a72b9142a60e
wildcatalum <efrenibarra@email.arizona.edu>
2020-06-08 15:47:27 -0700

    added chapter concepts employed

--- Isocelestriangle.cpp
+++ Isocelestriangle.cpp
@@ -1,3 +1,10 @@
+//Chapter 3
+//In this lab we will take user input and assign those values to our variables, that will be used to perform advanced calculations.
+//Be aware of Operator Precedence and rules of Associativity to arrive at correct results.
+//Introduce and use the C++ math library.
+//Let's continue developing our IsoscelesTriangle.cpp to incorporate the 
+//concepts of taking String datatype as input from the user and formatting our output to look nice.
+
 //Assignment 4 - 
 ///Purpose: Calculate the area and perimeter of an isoceles triangle given by base and height variables input by user via the console. 
 ///Input: base, height
@@ -8,33 +15,34 @@
 
 #include <iostream>
 #include <string>
-#inlcude <cmath>  //need for C++ math library
+#include <cmath>
 using namespace std;
 
 int main()
 {
     //delcare program purpose
-    cout << "This program calculates the area and perimeter of an isoceles triangle in meters";
+    cout << "This program calculates the area and perimeter of an isoceles triangle in meters\n";
 
     //get triangle base and height
-    cout << "Enter triangle base and height separated by a space";
-    cin << base << height; 
+    double base, height;
+    cout << "Enter triangle base and height separated by a space\n";
+    cin >> base >> height; 
 
     //calculate triangle length using pythgorean theorem
-    double base = base / 2
-    double length = sqrt(pow((base), 2.0) + pow(height, 2.0))
+    base = base / 2;
+    double length = sqrt(pow((base), 2.0) + pow(height, 2.0));
 
     //calculate area of triangle
-    double area = (1/2) * base * height
+    double area = (base * height);
 
     //calculate perimeter of triangle
-    double perimeter = length + base + height
+    double perimeter = length + base + height;
 
     //Message displaying the area
-    cout << "The traingle area is: " << area <<  "m^2\n";
+    cout << "The traingle area is: " << area <<  " m^2\n";
 
     //Message displaying the perimeter
-     cout << "The traingle perimeter is: " << perimeter <<  "m\n";
-     return 0
+     cout << "The traingle perimeter is: " << perimeter <<  " m\n";
+     return 0;
 }
 
