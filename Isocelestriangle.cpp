d54a4f8e57bd90f8a4dcbf1f0d98d97e55944361
wildcatalum <efrenibarra@email.arizona.edu>
2020-06-08 20:02:19 -0700

    added part two of this code. User input for three triangle calculation and output formatting

--- Isocelestriangle.cpp
+++ Isocelestriangle.cpp
@@ -15,34 +15,77 @@
 
 #include <iostream>
 #include <string>
-#include <cmath>
+#include <cmath> //required directive for c++ math library
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     //delcare program purpose
-    cout << "This program calculates the area and perimeter of an isoceles triangle in meters\n";
+    cout << "This program calculates the area and perimeter of three isoceles triangles with units in meters\n";
 
-    //get triangle base and height
-    double base, height;
-    cout << "Enter triangle base and height separated by a space\n";
-    cin >> base >> height; 
+    //Get first triangle name, base and height
+    string triangle1;
+    double base1, height1;
+    cout << "Enter first triangle name\n";
+    cin >> triangle1;
+    cout << "Enter first triangle base and height separated by a space\n";
+    cin >> base1 >> height1; 
 
     //calculate triangle length using pythgorean theorem
-    base = base / 2;
-    double length = sqrt(pow((base), 2.0) + pow(height, 2.0));
+    base1 = base1 / 2;
+    double length1 = sqrt(pow((base1), 2.0) + pow(height1, 2.0));
 
     //calculate area of triangle
-    double area = (base * height);
+    double area1 = (base1 * height1);
 
     //calculate perimeter of triangle
-    double perimeter = length + base + height;
+    double perimeter1 = length1 + base1 + height1;
 
-    //Message displaying the area
-    cout << "The traingle area is: " << area <<  " m^2\n";
+    //Get second triangle name, base and height
+    string triangle2;
+    double base2, height2;
+    cout << "Enter second triangle name\n";
+    cin >> triangle2;
+    cout << "Enter second triangle base and height separated by a space\n";
+    cin >> base2 >> height2; 
 
-    //Message displaying the perimeter
-     cout << "The traingle perimeter is: " << perimeter <<  " m\n";
-     return 0;
+    //calculate triangle length using pythgorean theorem
+    base1 = base2 / 2;
+    double length2 = sqrt(pow((base2), 2.0) + pow(height2, 2.0));
+
+    //calculate area of triangle
+    double area2 = (base2 * height2);
+
+    //calculate perimeter of triangle
+    double perimeter2 = length2 + base2 + height2;
+
+    //Get third triangle name, base and height
+    string triangle3;
+    double base3, height3;
+    cout << "Enter third triangle name:\n";
+    cin >> triangle3;
+    cout << "Enter third triangle base and height separated by a space:\n";
+    cin >> base3 >> height3; 
+
+    //calculate triangle length using pythgorean theorem
+    base3 = base3 / 2;
+    double length3 = sqrt(pow((base3), 2.0) + pow(height3, 2.0));
+
+    //calculate area of triangle
+    double area3 = (base3 * height3);
+
+    //calculate perimeter of triangle
+    double perimeter3 = length3 + base3 + height3;
+    
+    //Display formatted column output of triangle data with each column width equal to as many spaces as characters in largest output string plus one space to ease reading.
+    int cw = 16;
+    cout << setprecision(4) << fixed;    //set precision/fixed manipulator to format numbers to 4 decimal places.
+    cout << setw(cw) << "Name" << setw(cw) << "Base" << setw(cw) << "Height" << setw(cw) << "Area" << setw(cw) << "Perimeter" << "\n";
+    cout << setw(cw) << triangle1 << setw(cw) << base1*2 << setw(cw) << height1 << setw(cw) << area1 << setw(cw) << perimeter1 << "\n"; //base multiplied to return entered parameter
+    cout << setw(cw) << triangle2 << setw(cw) << base2*2 << setw(cw) << height2 << setw(cw) << area2 << setw(cw) << perimeter2 << "\n"; //setw() should be included before any output object so set the width of the field that object will be printed in
+    cout << setw(cw) << triangle3 << setw(cw) << base3*2 << setw(cw) << height3 << setw(cw) << area3 << setw(cw) << perimeter3 << "\n";
+return 0;   
 }
+
 
